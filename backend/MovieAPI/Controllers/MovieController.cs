5b40a1c61d023503cdfc8ca8a711610cf3ac5131
nathanconrad1 <96167711+nathanconrad1@users.noreply.github.com>
2023-04-06 10:28:17 -0600

    Add files via upload

---
+++ backend/MovieAPI/Controllers/MovieController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MovieAPI.Models;
+using System.Security.Cryptography.Xml;
+
+namespace MovieAPI.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class MovieController : Controller
+    {
+        private MoviesDbContext context;
+        public MovieController(MoviesDbContext temp) {
+            context = temp;
+        }
+        public IEnumerable<Movie> Get()
+        {
+            
+
+            return context.Movies
+                .Where(x => x.Edited == "Yes")
+                .OrderBy(x => x.Title)
+                .ToArray();
+        }
+    }
+}
---
+++ backend/MovieAPI/Models/MoviesDbContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace MovieAPI.Models
+{
+    public class MoviesDbContext : DbContext
+    {
+        public MoviesDbContext (DbContextOptions<MoviesDbContext> options) : base(options)
+        { 
+        }
+
+        public DbSet<Movie> Movies { get; set;}
+    }
+}
